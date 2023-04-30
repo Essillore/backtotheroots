e8dd250da52550bc56ab0187040618e111a299c2
Tuukka Pitkänen <pitkanentuukka@gmail.com>
2023-04-30 17:23:02 +0300

    added to grid, changed nutrient randomization

--- BackToTheRoots/Assets/Scripts/EarthStats.cs
+++ BackToTheRoots/Assets/Scripts/EarthStats.cs
@@ -9,21 +9,37 @@ public class EarthStats : MonoBehaviour
     public float nitrogenInTile = 15f;
     public float calciumInTile = 5f;
     public float potassiumInTile = 3f;
+    public GridManager gridManager;
+    public Vector2Int gridPosition;
+        private Vector2Int tempVector2;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        if(gameObject.transform.position.y > 0)
+        int x = Mathf.RoundToInt(gameObject.transform.position.x);
+        int y = Mathf.RoundToInt(gameObject.transform.position.y);
+        tempVector2 = new Vector2Int(x, y);
+        gridPosition = tempVector2;
+        gridManager.RegisterObject(tempVector2, gameObject);
+
+        // at surface, the range is 110-170, at 0, the range is 60-120, at -6 the range is 0-60
+        phosphorusInTile = (gridPosition.y * 10 ) + Random.Range(60f, 120f);
+        // at surface, the range is 150-220, at 0, the range is 90-160, at -6 the range is 30-100
+        nitrogenInTile = (gridPosition.y * 10) + Random.Range(90f, 160f);
+
+        /*if (gameObject.gridPosition.y>0)s
         {
             phosphorusInTile = Random.Range(20f, 50f);
             nitrogenInTile = Random.Range(30f, 80f);
-        } else
-            {
+        }
+        else
+        {
             phosphorusInTile = Random.Range(1f, 20f);
             nitrogenInTile = Random.Range(3f, 50f);
-        }
-
+        }*/
+        
+              
         waterInTile = Random.Range(1f, 20f);
         calciumInTile = Random.Range(3f, 10f);
         potassiumInTile = Random.Range(3f, 10f);
