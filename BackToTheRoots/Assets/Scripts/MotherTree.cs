64932638d6383951bc24fc8a233b1212f3a498a4
Essillore <riikka.pehkonen@me.com>
2023-05-01 15:47:45 +0300

    mothertree can get nutrients from roots

---
+++ BackToTheRoots/Assets/Scripts/MotherTree.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherTree : MonoBehaviour
+{
+    public float waterInTree = 30f;
+    public float phosphorusInTree = 7f;
+    public float nitrogenInTree = 15f;
+    public float calciumInTree = 5f;
+    public float potassiumInTree = 3f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- BackToTheRoots/Assets/Scripts/RootPiece.cs
+++ BackToTheRoots/Assets/Scripts/RootPiece.cs
@@ -8,6 +8,8 @@ public class RootPiece : MonoBehaviour
     public Vector2Int gridPosition;
     private Vector2Int tempVector2;
     public EarthStats earthstats;
+    public GameObject motherTree;
+    public MotherTree motherTreeScript;
 
     private float waterInRootpiece = 0f;
     private float phosphorusInRootpiece = 0f;
@@ -15,12 +17,13 @@ public class RootPiece : MonoBehaviour
     private float calciumInRootpiece = 0f;
     private float potassiumInRootpiece = 0f;
 
+    public Vector2 vectorToMotherTree;
+    public Vector2Int vToMotherTree;
+    public float distanceToMotherTree = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-
-
     int x = Mathf.RoundToInt(gameObject.transform.position.x);
         int y = Mathf.RoundToInt(gameObject.transform.position.y);
         tempVector2 = new Vector2Int(x, y);
@@ -33,6 +36,15 @@ public class RootPiece : MonoBehaviour
 
         earthstats.RootHasBeenAttached();
 
+        motherTreeScript = motherTree.GetComponent<MotherTree>();
+
+        StartCoroutine(AbsorbRatio(1f));
+
+        vectorToMotherTree = gameObject.transform.position - motherTree.transform.position;
+        vToMotherTree = Vector2Int.CeilToInt(vectorToMotherTree);
+        distanceToMotherTree = vToMotherTree.magnitude;
+
+        StartCoroutine(SendNutrientsToMotherRatio(distanceToMotherTree));
 
     }
 
@@ -40,7 +52,66 @@ public class RootPiece : MonoBehaviour
     void Update()
     {
         
+
     }
+    public IEnumerator SendNutrientsToMotherRatio(float howOften)
+    {
+            float amountToSend = 0.1f;
+            for (int i = 0; i <= 120; i +=1)
+            {
+            yield return new WaitForSeconds(howOften);
+            SendNutrientsToMotherTree(amountToSend);
+            print("Sent nutrients" + amountToSend);
+        }
+        yield break;
+    }
+
+    public IEnumerator AbsorbRatio(float howOften)
+    {
+        int upto = 120;
+        if (earthstats.phosphorusInTile > 1)
+        {
+            for (int i = 0; i <= upto; i += 1)
+            {
+                yield return new WaitForSeconds(howOften);
+                RootAbsorbsNutrients();
+            }
+        } else
+        {
+            yield break;
+        }
+       
+        
+    }
+
+    public void SendNutrientsToMotherTree(float amount)
+    {
+        if (waterInRootpiece>0)
+        {
+            waterInRootpiece -= amount;
+            motherTreeScript.waterInTree += amount;
+        }
+        if (phosphorusInRootpiece > 0)
+        {
+            phosphorusInRootpiece -= amount;
+            motherTreeScript.phosphorusInTree += amount;
+        }
+        if (nitrogenInRootpiece > 0)
+        {
+            nitrogenInRootpiece -= amount;
+            motherTreeScript.nitrogenInTree += amount;
+        }
+        if (calciumInRootpiece > 0)
+        {
+            calciumInRootpiece -= amount;
+            motherTreeScript.calciumInTree += amount;
+        }
+        if (potassiumInRootpiece > 0)
+        {
+            potassiumInRootpiece -= amount;
+            motherTreeScript.potassiumInTree += amount;
+        }
+}
 
     public void RootAbsorbsNutrients()
     {
