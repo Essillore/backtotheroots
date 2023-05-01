360dc74b3355c2ad0aebbf15b63938e6d60f5395
Essillore <riikka.pehkonen@me.com>
2023-05-01 20:49:30 +0300

    mousehower Icon

--- BackToTheRoots/Assets/Scripts/MotherTree.cs
+++ BackToTheRoots/Assets/Scripts/MotherTree.cs
@@ -18,7 +18,7 @@ public class MotherTree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(UpdateUI());
     }
 
     // Update is called once per frame
